7eade809d77922ab5fa5dc967bc7939258a3456d
Angel Luis R <angelluisrosario12345@gmail.com>
2026-08-10 17:53:46 -0400

    HU05: Vínculo directo y precarga de coordenadas a aplicaciones externas de navegación (Google Maps y Waze)

--- Models/RouteStop.cs
+++ Models/RouteStop.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace OrionMVP.Models
 {
     public class RouteStop
@@ -24,5 +26,26 @@ namespace OrionMVP.Models
         public string? CancellationReason { get; set; }
 
         public string ExternalNavUrl { get; set; } = string.Empty;
+
+        // External Navigation URL Generators (HU05)
+        public string GetGoogleMapsUrl()
+        {
+            if (Latitude != 0 && Longitude != 0)
+            {
+                return $"https://www.google.com/maps/search/?api=1&query={Latitude.ToString(System.Globalization.CultureInfo.InvariantCulture)},{Longitude.ToString(System.Globalization.CultureInfo.InvariantCulture)}";
+            }
+            var encodedAddress = Uri.EscapeDataString(FullAddress);
+            return $"https://www.google.com/maps/search/?api=1&query={encodedAddress}";
+        }
+
+        public string GetWazeUrl()
+        {
+            if (Latitude != 0 && Longitude != 0)
+            {
+                return $"https://waze.com/ul?ll={Latitude.ToString(System.Globalization.CultureInfo.InvariantCulture)},{Longitude.ToString(System.Globalization.CultureInfo.InvariantCulture)}&navigate=yes";
+            }
+            var encodedAddress = Uri.EscapeDataString(FullAddress);
+            return $"https://waze.com/ul?q={encodedAddress}&navigate=yes";
+        }
     }
 }
